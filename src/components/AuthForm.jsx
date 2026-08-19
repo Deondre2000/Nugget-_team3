842026c142fb1823fc0833146f6769f3250170eb
deondre butler <deondrebutler20@gmail.com>
2026-08-19 01:38:24 -0400

    updated the create profile almost finished with it

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -5,14 +5,19 @@ function AuthForm({ authMode, currentAuth, handleAuthSubmit }) {
     <div className="auth__content">
       {authMode === "signin" ? (
         <form className="auth__form" onSubmit={handleAuthSubmit}>
-          <label htmlFor="signin-email">Email</label>
+          <label className="auth__form-label" htmlFor="signin-email">
+            Email
+          </label>
           <input
+            className="auth__form-input"
             id="signin-email"
             type="email"
             placeholder="Enter your email"
           />
 
-          <label htmlFor="signin-password">Password</label>
+          <label className="auth__form-label" htmlFor="signin-password">
+            Password
+          </label>
           <input
             id="signin-password"
             type="password"
@@ -25,21 +30,27 @@ function AuthForm({ authMode, currentAuth, handleAuthSubmit }) {
         </form>
       ) : (
         <form className="auth__form" onSubmit={handleAuthSubmit}>
-          <label htmlFor="signup-email">Email</label>
+          <label className="auth__form-label" htmlFor="signup-email">
+            Email
+          </label>
           <input
             id="signup-email"
             type="email"
             placeholder="Enter your email"
           />
 
-          <label htmlFor="signup-password">Password</label>
+          <label className="auth__form-label" htmlFor="signup-password">
+            Password
+          </label>
           <input
             id="signup-password"
             type="password"
             placeholder="Create a password"
           />
 
-          <label htmlFor="signup-confirm-password">Confirm Password</label>
+          <label className="auth__form-label" htmlFor="signup-confirm-password">
+            Confirm Password
+          </label>
           <input
             id="signup-confirm-password"
             type="password"
